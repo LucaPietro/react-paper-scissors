0968affee2bf67777c965615d501bcc50be842fd
LucaPietro <lucabaronpietro@gmail.com>
2020-04-15 21:19:52 -0300

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+
+import { Button, Container, GameWindow, Header, GameScreen, GameOutput, PlayerMoveOutput, AiMoveOutput, ResultOutput } from './styles';
+import { GiRock, GiPaper, GiScissors } from 'react-icons/gi';
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [result, setResult] = useState('');
+  const [playerMove, setPlayerMove] = useState(0);
+  const [aiMove, setAiMove] = useState(0);
+
+  const moveDict = {
+    0: ['Rock', <GiRock size={70} />],
+    1: ['Paper', <GiPaper size={70} />],
+    2: ['Scissors', <GiScissors size={70} />],
+  };
+
+  const handleMoveCallback = useCallback(async (playerMove) => {
+    await setAiMove(Math.floor(Math.random() * 3));
+    setGameStarted(true);
+    setPlayerMove(playerMove);
+    console.log(playerMove, aiMove)
+
+    if (playerMove === aiMove) {
+      console.log(result)
+      return setResult('Draw')
+      console.log(result)
+    }
+
+    else if (playerMove === (aiMove + 1) || playerMove === (aiMove - 2)) {
+      console.log(result)
+      return setResult('Win')
+      console.log(result)
+    }
+
+    else {
+      console.log(result)
+      return setResult('Lost')
+      console.log(result)
+    }
+  }, [gameStarted, result, playerMove, aiMove])
+  console.log(result)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Container>
+        <GameWindow>
+          <Header>
+            React, Paper, Scissors
+          </Header>
+          <hr color={`#FFFFAA`} />
+          <GameScreen>
+            {!gameStarted && <span>Clique em um dos botões abaixo para iniciar o jogo.</span>}
+            {
+              gameStarted &&
+              <GameOutput>
+                <PlayerMoveOutput>
+                  <span>{moveDict[playerMove][1]}</span>
+                  <br />
+                  <span>Your Move: {moveDict[playerMove][0]}</span>
+                </PlayerMoveOutput>
+                <AiMoveOutput>
+                  <span>{moveDict[aiMove][1]}</span>
+                  <br />
+                  <span>AI Move: {moveDict[aiMove][0]}</span>
+                </AiMoveOutput>
+                <ResultOutput>
+                  <span>Result: {result}</span>
+                </ResultOutput>
+              </GameOutput>
+            }
+          </GameScreen>
+          <br />
+          <Button onClick={() => handleMoveCallback(0)}>Rock</Button>
+          <Button onClick={() => handleMoveCallback(1)}>Paper</Button>
+          <Button onClick={() => handleMoveCallback(2)}>Scissors</Button>
+        </GameWindow>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </Container>
+    </div >
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+  font: 400 18px Roboto, sans-serif;
+  /*background: #FF69B4;*/
+  background: linear-gradient(45deg, rgba(102,255,255,1) 0%, rgba(255,102,255,1) 100%);
+`
+
+export const GameWindow = styled.div`
+  text-align: center;
+  width: 60%;
+  height: 70vh;
+  border-radius: 10px;
+  background: rgba(255,255,255,1);
+`
+export const GameScreen = styled.div`
+  margin: 20px auto auto auto;
+  display: flex;
+  width: 80%;
+  height: 40vh;
+  border: 1px solid rgba(0,0,0,0.5);
+  border-radius: 10px;
+  color: rgba(0,0,0,0.5);
+`
+
+export const GameOutput = styled.div`
+  width: 100%;
+  display: grid;
+  place-content: stretch;
+  place-items: center;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 70% 30%;
+  grid-template-areas: 
+    "player-move ai-move"
+    "result result";
+`
+
+export const PlayerMoveOutput = styled.div`
+  grid-area: player-move;
+`
+export const AiMoveOutput = styled.div`
+  grid-area: ai-move;
+`
+
+export const ResultOutput = styled.div`
+  grid-area: result;
+  font-size: 30px;
+  margin-bottom: 20px;
+`
+
+export const Header = styled.h1`
+  font-size: 40px;
+  text-align: center;
+  margin: 10px;
+  color: rgba(255,102,255,0.7);
+`
+
+export const Button = styled.a`
+  display: inline-block;
+  border-radius: 10px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  text-align: center;
+  width: 7rem;
+  border: 3px solid rgba(255,255,255,1);
+  background: linear-gradient(45deg, rgba(102,255,255,0.7) 0%, rgba(255,102,255,0.7) 100%);
+  color: white;
+  cursor: pointer;
+  transition: opacity 0.2s;
+  &:hover {
+    border: 3px solid rgba(0,0,0,0.5);
+  };
+`
